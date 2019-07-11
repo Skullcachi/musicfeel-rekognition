2dbb525b7dbf3ecc425a4a164110e45be294c036
Sergio Molina <cachigua@gmail.com>
2019-07-11 11:01:10 -0600

    Update index.js
    added cors library

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-
+const cors = require('cors);
 const app = express();
 
 //Importando Rutas
@@ -14,6 +14,7 @@ app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(cors());
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
